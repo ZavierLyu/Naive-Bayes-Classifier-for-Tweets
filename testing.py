1a76356af02706dfc0f3623dc6975fb3c0bffb07
ZavierLyu <xzavierlv@outlook.com>
2019-05-04 00:19:07 +0800

    delete trivial codes and add report

--- testing.py
+++
@@ -1,9 +0,0 @@
-from training import *
-
-
-train_dic = unigram("data\\labeled-data-singlelabels-train.csv")
-
-
-def testing(filename, train_dic):
-    test_df = read_data(filename)
-    test_df["prediction"] = test_df.apply(lambda row: predict())
